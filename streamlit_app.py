5ab25de04b76ab206ca7a97925767bb4258f7169
ulasnowflake <104941389+ulasnowflake@users.noreply.github.com>
2022-05-04 14:28:52 -0700

    Update streamlit_app.py
    Added an option to select fruits from the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit
 import pandas
 
-streamlit.title('My Streamlit App')
+streamlit.title('My Streamlit Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🍳 Eggs')
 streamlit.text('🥗 Veggies')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)

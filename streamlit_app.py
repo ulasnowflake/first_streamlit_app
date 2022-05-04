4ab536f82f88b31199b4630ced350bd7391daeae
ulasnowflake <104941389+ulasnowflake@users.noreply.github.com>
2022-05-04 14:26:21 -0700

    Update streamlit_app.py
    Uploaded and displayed fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('My Streamlit App')
 streamlit.header('Breakfast menu')
 streamlit.text('🍳 Eggs')
 streamlit.text('🥗 Veggies')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)

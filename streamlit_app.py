0abe93997bad4bd156b46455df898241da05c93b
ulasnowflake <104941389+ulasnowflake@users.noreply.github.com>
2022-05-05 17:09:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about? 🍐 🍌 🍉', 'Kiwi') 
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 

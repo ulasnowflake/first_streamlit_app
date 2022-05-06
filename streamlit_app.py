a1af0b3e555ac13c80839c0b9340daa7f6cf259c
ulasnowflake <104941389+ulasnowflake@users.noreply.github.com>
2022-05-06 15:26:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,15 @@ import pandas
 import requests
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake! ❄️ ❄️ ❄️:"
+streamlit.text(my_data_row) 
+
+
+
 streamlit.title('My Streamlit Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🍳 Eggs')
